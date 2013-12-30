c8c91574d1d9d5f34879b0a52db92b04d45c0426
Filipp Ozinov <ozinov@quantion.ru>
2013-12-30 20:47:31 +0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+wget https://github.com/bakwc/vim/archive/master.zip
+unzip master.zip
+rm ~/.vimrc
+rm -rf ~/.vim/
+cp vim-master/.vimrc ~/.vimrc
+cp -r vim-master/.vim/ ~/.vim/
+rm -rf vim-master master.zip
+echo "Done!"
+
