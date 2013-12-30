23ac1c26f1939e14ef94372d3eb3ee9b9e961c9b
Filipp Ozinov <ozinov@quantion.ru>
2013-12-30 20:53:50 +0400

    fixed parameter - it should be before arguments

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-wget https://github.com/bakwc/vim/archive/master.zip --no-check-certificate
+wget --no-check-certificate https://github.com/bakwc/vim/archive/master.zip
 unzip master.zip
 rm ~/.vimrc
 rm -rf ~/.vim/
